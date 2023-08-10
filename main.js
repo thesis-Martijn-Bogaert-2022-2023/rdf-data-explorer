3ad73935daa36213aff10896f6ede50e8e4a6643
Martijn Bogaert <martijn.bogaert@ugent.be>
2023-08-10 11:49:47 +0200

    Add vis network

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 import './style.css';
+import { DataSet, Network } from 'vis-network/standalone/esm/vis-network';
+
+const nodes = new DataSet();
+const edges = new DataSet();
+
+const data = {
+	nodes: nodes,
+	edges: edges,
+};
+
+const options = {
+	layout: {
+		hierarchical: {
+			direction: 'LR', // Left to right
+		},
+	},
+	edges: {
+		arrows: 'to',
+	},
+};
+
+const container = document.getElementById('network');
+const network = new Network(container, data, options);
+
+// Add nodes
+nodes.add([
+	{ id: 1, label: 'Node 1' },
+	{ id: 2, label: 'Node 2' },
+	{ id: 3, label: 'Node 3' },
+]);
+
+// Add edges
+edges.add([
+	{ from: 1, to: 2, label: 'Edge 1' },
+	{ from: 2, to: 3, label: 'Edge 2' },
+]);
